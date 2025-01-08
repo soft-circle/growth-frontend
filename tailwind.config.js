0e6a06ad4eaace566c882fc0977adfa8eb5ff7e7
hyemin jang <hyemin.jang@tridge.com>
2025-01-08 09:27:36 +0900

    Add DataTable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    darkMode: ["class"],
-    content: [],
+  darkMode: ["class"],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
   	extend: {
   		borderRadius: {
