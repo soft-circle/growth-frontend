d3be9d8b1c73780c2c8faac320c81473e7895661
Hyemin Jang <janghyemin@janghyemin-ui-noteubug.local>
2025-03-27 23:05:50 +0900

    Enhance NumberField

--- src/components/NumberField/index.tsx
+++ src/components/NumberField/index.tsx
@@ -1,27 +1,85 @@
-import { Input } from '../ui/input';
-import FormFieldLabeled, {
-  FormFieldLabeledProps,
-} from '../FormFieldLabeled';
+import { isNumber, isNil } from 'lodash';
+
+import FormField, {
+  FormFieldProps,
+} from '@/components/FormField';
+import {
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from '@/components/Icon';
+import { Input } from '@/components/ui/input';
 
 export interface NumberFieldProps extends
-  Omit<React.ComponentProps<'input'>, 'defaultValue' | 'name'>,
-  Omit<FormFieldLabeledProps<number>, 'children'> {}
+  Omit<React.ComponentProps<'input'>, 'name' | 'defaultValue'>,
+  Omit<FormFieldProps<number>, 'children'> {
+  min?: number;
+  max?: number;
+  step?: number;
+}
 
 export default function NumberField({
+  name,
+  defaultValue,
+  rules,
+  shouldUnregister,
+  control,
+  disabled,
+  min = Number.MIN_SAFE_INTEGER,
+  max = Number.MAX_SAFE_INTEGER,
+  step = 1,
   ...props
 }: NumberFieldProps) {
   return (
-    <FormFieldLabeled<number>
+    <FormField
+      name={name}
+      defaultValue={defaultValue}
+      rules={rules}
+      shouldUnregister={shouldUnregister}
+      control={control}
+      disabled={disabled}
       {...props}
     >
       {({ field }) => (
-        <Input
-          className="bg-white"
-          {...props}
-          {...field}
-          value={field.value ? Number(`${field.value}`.replace(/[^0-9]/g, '')) : ''}
-        />
+        <div className="relative">
+          <Input
+            type="number"
+            min={min}
+            max={max}
+            step={step}
+            className="bg-white"
+            {...props}
+            {...field}
+            value={isNil(field.value) ? '' : field.value}
+          />
+          <div className="absolute top-0 bottom-0 right-0 flex flex-col m-px">
+            <button
+              type="button"
+              className="group block flex-1 px-1 border-l border-gray-200 rounded-tr-md hover:bg-accent disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-ring"
+              disabled={isNumber(max) && (field.value || 0) >= max}
+              onClick={() => {
+                field.onChange((field.value || 0) + step);
+              }}
+            >
+              <ChevronUpIcon
+                className="!size-3 group-disabled:[&_path]:stroke-gray-400"
+              />
+            </button>
+            <div className="h-px bg-gray-200" />
+            <button
+              type="button"
+              className="group block flex-1 px-1 border-l border-gray-200 rounded-br-md hover:bg-accent disabled:pointer-events-none focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-inset focus-visible:ring-ring"
+              disabled={isNumber(min) && (field.value || 0) <= min}
+              onClick={() => {
+                field.onChange((field.value || 0) - step);
+              }}
+            >
+              <ChevronDownIcon
+                className="!size-3 group-disabled:[&_path]:stroke-gray-400"
+              />
+            </button>
+          </div>
+        </div>
       )}
-    </FormFieldLabeled>
+    </FormField>
   );
 }
